ca86f6e99b46b5b71230292c9d7731b984d4a2c1
lijide <1916625156@qq.com>
2019-10-11 18:49:45 +0800

    调整目录结构

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue' // 导入Vue实例
+import VueRouter from 'vue-router' // 导入router实例
+
+Vue.use(VueRouter) // router注册全局共享
+
+const router = new VueRouter({
+  // 配置路由表
+  routes: [
+
+  ]
+})
+export default router
